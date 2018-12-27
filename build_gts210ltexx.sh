07feab2958914fdffeeb41e50d978135702f3caa
MSe <mse1969@posteo.de>
2018-12-27 18:58:55 +0100

    GTS210LTEXX: Optional signed microG build
    Change-Id: If91e02984a3db1c741eded2820967ae093c2aa73

--- build_gts210ltexx.sh
+++ build_gts210ltexx.sh
@@ -5,5 +5,5 @@ export CCACHE_DIR=~/.ccache14
 export OUT_DIR_COMMON_BASE=~/out-android
 
 # Use pre-defined build script
-source z_patches/build_gts210ltexx.sh
+source z_patches/build_gts210ltexx.sh $1
 
