ba46dc68e2e385dbbe141b03edf7f572ce8ef7df
MSe <mse1969@posteo.de>
2020-09-27 14:46:54 +0200

    switch-script: Track own system/core
    Change-Id: Ia6a6b8e3221b94a7af0fd95e77c5adae1756167e

--- switch_microG.sh
+++ switch_microG.sh
@@ -85,6 +85,7 @@ switch_branches $BRANCH1 frameworks/base
 switch_branches $BRANCH1 frameworks/native
 switch_branches $BRANCH1 packages/apps/Settings
 switch_branches $BRANCH3 packages/apps/SetupWizard
+switch_branches $BRANCH4 system/core
 switch_branches $BRANCH4 system/netd
 switch_branches $BRANCH1 system/sepolicy
 switch_branches $BRANCH1 vendor/cm
